14bd54657f09f21df6cea83138030aa693ffc967
dailydiy <rupin.chheda@gmail.com>
2019-10-18 17:29:04 +0530

    created basic function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from onlineservices.TTS import TTS
 def main(request):
 	sentence="My Name is Rupin"
-	newTTSObject=TTS(TTS.SERVICE_PROVIDER_GOOGLE)
-	audioStream=newTTSObject.getTTSAudio(sentence)
+	newTTSObject=TTS('GOOG')
+	audioStream=newTTSObject.convertTTSGoogle(sentence)
 	exit()
