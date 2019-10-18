2532e9a3d1457d82ad7b28a11527c34973917f5d
dailydiy <rupin.chheda@gmail.com>
2019-10-18 23:14:30 +0530

    Benchmarking

--- main.py
+++ main.py
@@ -3,31 +3,51 @@ from google.cloud import storage
 from pydub import AudioSegment
 import json
 from io import BytesIO
-
+import time
 
 
 
 
 def main(request):
-	bucket_name="written-audio-files"
+	
 	#sentence="Let us Learn How to Design a PCB using Easy E D A"
 	print(request)
 	jsonobject=request.get_json()
 	newTTSObject=TTS()
+	t0=time.time()
 	audioStream=newTTSObject.convertTTSGoogle(jsonobject)
+	t1=time.time()
+	print("TTS Took: " +str(t1-t0))
+	newTTSObject=None
+
 	filename=jsonobject.get('filename')
 	filename_with_extension=filename+".mp3"
 
 
 	# Instantiates a client
+	t2=time.time()
 	storage_client = storage.Client()
+	bucket_name="written-audio-files"
 	bucket = storage_client.get_bucket(bucket_name) 
 	blob = bucket.blob(filename_with_extension)
 	blob.upload_from_string(audioStream)
+	t3=time.time()
+	print("Storage Took: " +str(t3-t2))
+
+	storage_client=None
+	bucket=None
+	blob=None
+
 	output={}
+	
+	
+
 	output['file_url']='https://storage.cloud.google.com/'+bucket_name+'/'+filename_with_extension
+	t4=time.time()
 	current_audio=AudioSegment.from_mp3(BytesIO(audioStream))	
 	output['duration']=current_audio.duration_seconds
+	t5=time.time()
+	print("Track Duration Took: " +str(t5-t4))
 	jsonreturnvalue=json.dumps(output)
 	return jsonreturnvalue
 
